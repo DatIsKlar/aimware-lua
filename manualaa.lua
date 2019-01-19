34e64f5db001af4c8f567128114dc426bb1b755a
DatIsKlar <46826117+DatIsKlar@users.noreply.github.com>
2019-01-19 12:58:48 +0100

    Update manualaa.lua

--- manualaa.lua
+++ manualaa.lua
@@ -306,11 +306,11 @@ local function arrow()
         end
     end
 end
-local SCRIPT_FILE_NAME = GetScriptName();
+local SCRIPT_FILE_NAME = "manualaa.lua";
 local SCRIPT_FILE_ADDR = "https://raw.githubusercontent.com/DatIsKlar/aimware-lua/master/manualaa.lua";
 local VERSION_FILE_ADDR = "https://raw.githubusercontent.com/DatIsKlar/aimware-lua/master/maversion.txt";
 local VERSION_NUMBER = 3;
-
+local update_log ="https://raw.githubusercontent.com/DatIsKlar/update_log-aw/master/update_log_aa"
 
 local update_available = false;
 local version_check_done = false;
@@ -329,6 +329,8 @@ function manual_draw_event()
         else
             local new_version_content = http.Get(SCRIPT_FILE_ADDR);
             local old_script = file.Open(SCRIPT_FILE_NAME, "w");
+			local update_log = http.Get(update_link)
+			print(update_log)
             old_script:Write(new_version_content);
             old_script:Close();
             update_available = false;
